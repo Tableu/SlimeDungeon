95a1479025e0e9c95f88192ad730143b12c2fb0e
darthvader99999 <bhow245@gmail.com>
2023-07-27 23:01:53 -0700

    TMP package

---
+++ Assets/Scripts/UI/FormIcon.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FormIcon : MonoBehaviour
+{
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private Image image;
+
+    private void Start()
+    {
+        playerController.OnFormChange += OnFormChange;
+    }
+
+    private void OnFormChange()
+    {
+        image.sprite = playerController.Form.data.Icon;
+    }
+
+    private void OnDestroy()
+    {
+        playerController.OnFormChange -= OnFormChange;
+    }
+}
