dc0b5fc1607f3ec2e8e02eef993a09d88bde74b3
Brian How <bhow@sfu.ca>
2024-05-02 14:02:20 -0700

    Updating fog of war obstacle map when doors are opened

--- Assets/Scripts/Dungeon Level/Door.cs
+++ Assets/Scripts/Dungeon Level/Door.cs
@@ -1,4 +1,5 @@
 using System;
+using FischlWorks_FogWar;
 using UnityEngine;
 
 public class Door : MonoBehaviour
@@ -8,10 +9,26 @@ public class Door : MonoBehaviour
     [SerializeField] private new Collider collider;
     [SerializeField] private Collider leftCollider;
     [SerializeField] private Collider rightCollider;
+    [SerializeField] private Transform parent;
+    private csFogWar _fogWar;
+    private void Start()
+    {
+        // This part is meant to be modified following the project's scene structure later...
+        try
+        {
+            _fogWar = GameObject.Find("FogWar").GetComponent<csFogWar>();
+        }
+        catch
+        {
+            Debug.LogErrorFormat("Failed to fetch csFogWar component. " +
+                                 "Please rename the gameobject that the module is attached to as \"FogWar\", " +
+                                 "or change the implementation located in the csFogVisibilityAgent.cs script.");
+        }
+    }
 
     private void Open()
     {
-        int parentRotation = Math.Sign(transform.parent.rotation.eulerAngles.y);
+        int parentRotation = Math.Sign(parent.rotation.eulerAngles.y);
         if (parentRotation == 0)
             parentRotation = 1;
         leftDoor.transform.localRotation = Quaternion.Euler(0, 90*parentRotation, 0);
@@ -19,6 +36,26 @@ public class Door : MonoBehaviour
         leftCollider.enabled = false;
         rightCollider.enabled = false;
         collider.enabled = false;
+        Vector2Int levelCoords = _fogWar.WorldToLevel(transform.position);
+        for (int x = 0; x < _fogWar._UnitScale+1; x++)
+        {
+            switch (parent.rotation.eulerAngles.y)
+            {
+                case 0:
+                    _fogWar.levelData[levelCoords.x][levelCoords.y-x] = csFogWar.LevelColumn.ETileState.Empty;
+                    break;
+                case 90:
+                    _fogWar.levelData[levelCoords.x-x][levelCoords.y] = csFogWar.LevelColumn.ETileState.Empty;
+                    break;
+                case 180:
+                    _fogWar.levelData[levelCoords.x][levelCoords.y+x] = csFogWar.LevelColumn.ETileState.Empty;
+                    break;
+                case 270:
+                    _fogWar.levelData[levelCoords.x+x][levelCoords.y] = csFogWar.LevelColumn.ETileState.Empty;
+                    break;
+            }
+        }
+        
     }
 
     private void OnCollisionEnter(Collision other)
