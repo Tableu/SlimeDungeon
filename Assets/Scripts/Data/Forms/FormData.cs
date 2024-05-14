778395bcbdd09b099c60657be41d8857724b1a24
Brian How <bhow@sfu.ca>
2024-05-14 00:31:04 -0700

    Removing form icon data

--- Assets/Scripts/Data/Forms/FormData.cs
+++ Assets/Scripts/Data/Forms/FormData.cs
@@ -13,12 +13,10 @@ namespace Controller.Form
         [SerializeField] private float health;
         [SerializeField] private float speed;
         [SerializeField] private Vector2 maxVelocity;
-        [SerializeField] private float captureDifficulty = 1;
         
         [HeaderAttribute("References")]
         [SerializeField] private GameObject model;
         [FormerlySerializedAs("item")] [SerializeField] private GameObject capturedForm;
-        [SerializeField] private Sprite icon;
         [SerializeField] private Vector3 spellOffset;
         [SerializeField] private AttackData basicAttack;
         [SerializeField] private List<AttackData> spells;
@@ -27,11 +25,9 @@ namespace Controller.Form
         public float Health => health;
         public float Speed => speed;
         public Vector2 MaxVelocity => maxVelocity;
-        public float CaptureDifficulty => captureDifficulty;
         public GameObject Model => model;
         public Vector3 SpellOffset => spellOffset;
         public GameObject CapturedForm => capturedForm;
-        public Sprite Icon => icon;
         public AttackData BasicAttack => basicAttack;
         public List<AttackData> Spells => spells;
 
