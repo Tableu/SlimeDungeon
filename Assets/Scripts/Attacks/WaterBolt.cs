a0cc0be02300dca8d7fc79c183ce4b83678ff8e1
Brian How <bhow@sfu.ca>
2024-05-24 00:40:11 -0700

    Fixing waterbolt bouncing issue by adjusting door/wall colliders and checking for multiple collisions in the waterbolt script

--- Assets/Scripts/Attacks/WaterBolt.cs
+++ Assets/Scripts/Attacks/WaterBolt.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class WaterBolt : MonoBehaviour
@@ -10,6 +11,7 @@ public class WaterBolt : MonoBehaviour
     private Elements.Type _type;
     private int _maxBounces;
     private int _bounceCount = 0;
+    private float _lastBounceTimestamp;
 
     public void Initialize(float damage, float knockback,float hitStun, Vector3 force, Elements.Type type, int maxBounces)
     {
@@ -30,16 +32,24 @@ public class WaterBolt : MonoBehaviour
             damage.TakeDamage(_damage,_knockback*_force.normalized, _hitStun, _type);
             Destroy(gameObject);
         }
+
+        if (Math.Abs(Time.time - _lastBounceTimestamp) < 0.001)
+        {
+            return;
+        }
         
         if (_bounceCount < _maxBounces)
         {
             _bounceCount++;
             rigidbody.velocity = Vector3.Reflect(_force, other.GetContact(0).normal);
             _force = rigidbody.velocity;
+            _lastBounceTimestamp = Time.time;
         }
         else
         {
             Destroy(gameObject);
         }
+
+        
     }
 }
--- Assets/Scripts/Dungeon Level/Door.cs
+++ Assets/Scripts/Dungeon Level/Door.cs
@@ -6,9 +6,8 @@ public class Door : MonoBehaviour
 {
     [SerializeField] private GameObject leftDoor;
     [SerializeField] private GameObject rightDoor;
-    [SerializeField] private new Collider collider;
-    [SerializeField] private Collider leftCollider;
-    [SerializeField] private Collider rightCollider;
+    [SerializeField] private Collider doorCollider;
+    [SerializeField] private Collider frameCollider;
     [SerializeField] private Transform parent;
     private csFogWar _fogWar;
     private void Start()
@@ -33,9 +32,8 @@ public class Door : MonoBehaviour
             parentRotation = 1;
         leftDoor.transform.localRotation = Quaternion.Euler(0, 90*parentRotation, 0);
         rightDoor.transform.localRotation = Quaternion.Euler(0, -90*parentRotation, 0);
-        leftCollider.enabled = false;
-        rightCollider.enabled = false;
-        collider.enabled = false;
+        doorCollider.enabled = false;
+        frameCollider.enabled = false;
         Vector2Int levelCoords = _fogWar.WorldToLevel(transform.position);
         for (int x = 0; x < _fogWar._UnitScale+1; x++)
         {
