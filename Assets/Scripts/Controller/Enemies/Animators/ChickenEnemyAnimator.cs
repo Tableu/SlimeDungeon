094bf24c9aeda3b209eef9c208ecd4c25e44cf3e
Brian How <bhow@sfu.ca>
2024-06-07 00:15:41 -0700

    Adjusting enemy stats

--- Assets/Scripts/Controller/Enemies/Animators/ChickenEnemyAnimator.cs
+++ Assets/Scripts/Controller/Enemies/Animators/ChickenEnemyAnimator.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class ChickenEnemyAnimator : EnemyAnimator
 {
     public override void ChangeState(EnemyControllerState state)
